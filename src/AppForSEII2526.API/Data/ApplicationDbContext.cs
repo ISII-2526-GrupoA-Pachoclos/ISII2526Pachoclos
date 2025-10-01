ca57928717718deab1e56974992c40d0ff4357ab
ADRIAN DANIEL MECINAS DUMITRU <53708631+AdrianDanielMecinasDumitru@users.noreply.github.com>
2025-10-01 18:34:33 +0200

    Actualizar cambios commits de development a main (#19)
    * Añadido nuevo miembro 4 al grupo (#3)
    
    * 2 actualizar infoyml (#4)
    
    * Añadido nuevo miembro 4 al grupo
    
    * Añadido miembro 4 al grupo
    
    * Creación clase fabricante (#8)
    
    * Crear clase herramienta y atributos (#9)
    
    * Crear clase OfertaItem y atributos (#12)
    
    * ComprarItem (#13)
    
    * Crear clase Oferta y atributos (#16)
    
    * Crear clase Compra (#18)
    
    * 1 actualizar infoyml (#17)
    
    * Añadido nuevo miembro 4 al grupo
    
    * Añadido miembro 4 al grupo
    
    * Actualizado en project name "GrupoA"
    
    ---------
    
    Co-authored-by: jesussotos <jesus.sotos@alu.uclm.es>
    Co-authored-by: Marcos Villalba López <Marcos.Villalba@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,11 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Herramienta> Herramienta { get; set; }
+    public DbSet<fabricante> fabricante { get; set; }
+    public DbSet<ComprarItem> ComprarItem { get; set; }
+    public DbSet<OfertaItem> OfertaItem { get; set; }
+    public DbSet<Compra> Compra { get; set; }
+    public DbSet<Oferta> Oferta { get; set; }
+
 }
---
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -0,0 +1,40 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Compra
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 1)]
+        public string nombreCliente { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "No puede tener mas de 100 caracteres.", MinimumLength = 1)]
+        public string direccionEnvio { get; set; }
+
+
+        [StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 1)]
+        public string apellidoCliente { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime fechaCompra { get; set; }
+
+        [StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 1)]
+        public string correoElectronico { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        public float precioTotal { get; set; }
+
+        [StringLength(9, ErrorMessage = "No puede tener mas de 9 numeros.", MinimumLength = 1)]
+        public string telefono { get; set; }
+
+        public enum metodoPago
+        {
+            Efectivo,
+            TarjetaCredito,
+            PayPal
+        }
+    }
+}
