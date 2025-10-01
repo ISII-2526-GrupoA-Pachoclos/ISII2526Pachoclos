bb69fbbaf6272400be13bb11308c0a0c296309b7
jesussotos <jesus.sotos@alu.uclm.es>
2025-10-01 17:37:42 +0200

    ComprarItem (#13)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -7,5 +7,7 @@ namespace AppForSEII2526.API.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<Herramienta> Herramienta { get; set; }
     public DbSet<fabricante> fabricante { get; set; }
+    public DbSet<ComprarItem> ComprarItem { get; set; }
     public DbSet<OfertaItem> OfertaItem { get; set; }
+
 }
