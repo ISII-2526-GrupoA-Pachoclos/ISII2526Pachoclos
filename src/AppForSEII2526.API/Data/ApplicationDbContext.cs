1a920f1d7eeb9db6eafb04866cd47ca06c4b6162
ADRIAN DANIEL MECINAS DUMITRU <53708631+AdrianDanielMecinasDumitru@users.noreply.github.com>
2025-10-02 17:19:06 +0200

    27 modificar clase applicationuser (#45)
    * Actualizado el ApplicationUser con sus atributos
    
    * Actualizado clase applicationuser y atributos
    
    * actualizado bdcontext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -15,5 +15,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<ReparacionItem> ReparacionItem { get; set; }
     public DbSet<Reparacion> Reparacion { get; set; }
     public DbSet<ApplicationUser> ApplicationUser { get; set; }
-
 }
+
