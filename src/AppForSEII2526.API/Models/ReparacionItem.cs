7980192e5692e8d088e52f1e9dd56c6034e1058a
Marcos Villalba López <Marcos.Villalba@alu.uclm.es>
2025-10-03 19:48:48 +0200

    arreglados algunas restricciones del cdu2 reparar herramientas (#73)

--- src/AppForSEII2526.API/Models/ReparacionItem.cs
+++ src/AppForSEII2526.API/Models/ReparacionItem.cs
@@ -3,7 +3,7 @@
     [PrimaryKey(nameof(idHerramienta), nameof(idReparacion))]
     public class ReparacionItem
     {
-        [Required, Range(1, int.MaxValue, ErrorMessage = "El id debe ser un número positivo.")]
+        [Required, Range(1, int.MaxValue, ErrorMessage = "El id debe ser un número positivo mayor que 0.")]
         public int cantidad { get; set; }
 
         [StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 4)]
@@ -13,11 +13,13 @@
 
         public int idReparacion { get; set; }
 
+        [Required, Range(0.01, float.MaxValue, ErrorMessage = "El precio debe ser un valor positivo.")]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
         public float precio { get; set; }
 
         // Relación
         public Reparacion Reparacion { get; set; }
+
         public Herramienta Herramienta { get; set; }
         }
 }
