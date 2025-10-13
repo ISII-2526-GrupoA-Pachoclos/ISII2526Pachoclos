1d33ae606aad5c9ef72d5f5f0c9c1511fa0b069c
MARCOS VILLALBA LÓPEZ <Marcos.Villalba@alu.uclm.es>
2025-10-13 11:13:38 +0200

    actualizado parte 1

---
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; // Para que lea la base de datos
+
+        private readonly ILogger<HerramientasController> _logger; // para escribir en el log cualquier informacion cuando el sistema se ejecute
+
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IEnumerable<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> getHerramientasAlquilar()
+        {
+            IList<Herramienta> herramientas = await _context.Herramienta.ToListAsync();
+            return Ok(herramientas);
+        }
+    }
+}
