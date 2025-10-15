175543e0fa17c8d463f506f85a0ad719aa08f229
jesussotos <Jesus.Sotos@alu.uclm.es>
2025-10-15 13:07:19 +0200

    Método get de la seleccion

---
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -0,0 +1,60 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HerramientasController> _logger;
+
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("[accion]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllHerramientas()
+        {
+            IList<Herramienta> herramientas = await _context.Herramienta.ToListAsync();
+            return Ok(herramientas);
+
+        }
+
+
+        [HttpGet]
+        [Route("[accion]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas_sinDTOs() { 
+            IList<Herramienta> herramientas = await _context.Herramienta.ToListAsync();
+            return Ok(herramientas);
+
+        }
+        [HttpGet]
+        [Route("[accion]")]
+        [ProducesResponseType(typeof(IList<HerramientasParaComprarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientasParaComprar_conTodosLosDatos_DTO(float? filtroPrecio, string? filtroMaterial) {
+            
+            var herramientas = await _context.Herramienta
+                .Include(h => h.fabricante)
+                .Where(h => (h.precio <= filtroPrecio || (filtroPrecio==null))
+                && (h.material.Contains(filtroMaterial) ||  (filtroMaterial==null))
+                )
+                .OrderBy(h => h.nombre)
+                .Select(h => new HerramientasParaComprarDTO(h.id, h.nombre, h.material, h.fabricante.nombre, h.precio))
+                .ToListAsync();
+            return Ok(herramientas);
+
+        }
+
+
+
+    }
+}
---
+++ src/AppForSEII2526.API/Data/dbo.fabricante.data.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[fabricante] ON
+INSERT INTO [dbo].[fabricante] ([id], [nombre]) VALUES (1, N'Duviso')
+INSERT INTO [dbo].[fabricante] ([id], [nombre]) VALUES (2, N'Ferreteria Florencio')
+SET IDENTITY_INSERT [dbo].[fabricante] OFF
+
+
+SET IDENTITY_INSERT [dbo].[Herramienta] ON
+INSERT INTO [dbo].[Herramienta] ([id], [material], [nombre], [precio], [tiempoReparacion], [fabricanteid]) VALUES (4, N'hierro', N'destornillador', 12, N'12', 1)
+INSERT INTO [dbo].[Herramienta] ([id], [material], [nombre], [precio], [tiempoReparacion], [fabricanteid]) VALUES (5, N'hierro', N'martillo', 6, N'3', 1)
+INSERT INTO [dbo].[Herramienta] ([id], [material], [nombre], [precio], [tiempoReparacion], [fabricanteid]) VALUES (6, N'varios', N'motosierra', 150, N'20', 2)
+SET IDENTITY_INSERT [dbo].[Herramienta] OFF
