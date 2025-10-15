531ee109e8a7b45c1f505784caaab769d299ebc5
ADRIÁN DANIEL MECINAS DUMITRU <AdrianDaniel.Mecinas@alu.uclm.es>
2025-10-15 13:17:02 +0200

    creado dto cdu3

---
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -0,0 +1,49 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HerramientasController> _logger;
+
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        //Devuelve toda la información directamente de la BBDD
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Herramienta>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramienta_sinDTOs()
+        {
+            IList<Herramienta> herramientas = await _context.Herramienta.ToListAsync();
+            return Ok(herramientas);
+        }
+
+        //Devuelve todos los datos de las herramientas que se piden en el paso 2 del CU3 (Crear Ofertas)
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<HerramientasParaOfertasDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetHerramientas_conTodosLosDatos_DTO(float? filtroPrecio, string? filtroFabricante)
+        {
+            var herramientas = await _context.Herramienta
+                .Include(h => h.fabricante)
+                .Where(h =>
+                    (filtroPrecio == null || h.precio < filtroPrecio) &&
+                    (filtroFabricante == null || h.fabricante.nombre == filtroFabricante)
+                )
+                .OrderBy(herramientas => herramientas.fabricante.nombre)
+                .Select(h => new HerramientasParaOfertasDTO(h.id, h.material, h.nombre, h.precio, h.fabricante.nombre))
+                .ToListAsync();
+
+            return Ok(herramientas);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/Data/dbo.fabricante.data.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[fabricante] ON
+INSERT INTO [dbo].[fabricante] ([id], [nombre]) VALUES (1, N'Paco')
+INSERT INTO [dbo].[fabricante] ([id], [nombre]) VALUES (2, N'Jose')
+INSERT INTO [dbo].[fabricante] ([id], [nombre]) VALUES (3, N'Pepe')
+SET IDENTITY_INSERT [dbo].[fabricante] OFF
+
+
+SET IDENTITY_INSERT [dbo].[Herramienta] ON
+INSERT INTO [dbo].[Herramienta] ([id], [material], [nombre], [precio], [tiempoReparacion]) VALUES (2, N'Hierro', N'Llave Inglesa', 20, N'10')
+INSERT INTO [dbo].[Herramienta] ([id], [material], [nombre], [precio], [tiempoReparacion]) VALUES (3, N'Madera', N'Martillo', 15, N'10')
+SET IDENTITY_INSERT [dbo].[Herramienta] OFF
