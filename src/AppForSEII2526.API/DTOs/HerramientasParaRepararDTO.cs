8497f0fb185d69f2c7c05014677d9a85e2a40529
Marcos Villalba López <Marcos.Villalba@alu.uclm.es>
2025-10-15 13:12:12 +0200

    crear metodo getherramientas cd2 con dto (#115)
    squasheado!

---
+++ src/AppForSEII2526.API/DTOs/HerramientasParaRepararDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class HerramientasParaRepararDTO
+    {
+        [Key]
+        public int id { get; set; }
+
+        [StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 1)]
+        public string material { get; set; }
+
+        [Required, StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 1)]
+        public string nombre { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        public float precio { get; set; }
+
+        public string tiempoReparacion { get; set; }
+
+        public string fabricante { get; set; }
+
+        public HerramientasParaRepararDTO(int id, string material, string nombre, float precio, string tiempoReparacion, string fabricante)
+        {
+            this.id = id;
+            this.material = material;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.tiempoReparacion = tiempoReparacion;
+            this.fabricante = fabricante;
+        }
+    }
+}
