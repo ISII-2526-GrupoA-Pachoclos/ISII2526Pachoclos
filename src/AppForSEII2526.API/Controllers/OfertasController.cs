2d02fec5522619b7aa05c8c351749fa445f366db
ADRIÁN DANIEL MECINAS DUMITRU <AdrianDaniel.Mecinas@alu.uclm.es>
2025-10-19 11:20:29 +0200

    Iniciado creación del controlador OfertasController

---
+++ src/AppForSEII2526.API/Controllers/OfertasController.cs
@@ -0,0 +1,31 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfertasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<OfertasController> _logger;
+
+        public OfertasController(ApplicationDbContext context, ILogger<OfertasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<OfertasDTO>) , (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetDetallesParaOfertas_conTodosLosDatos_DTO()
+        {
+            var ofertas = await _context.Ofertas
+                
+                .ToListAsync();
+            return Ok(ofertas);
+        }
+    }
+}
--- src/AppForSEII2526.API/DTOs/OfertasDTO.cs
+++ src/AppForSEII2526.API/DTOs/OfertasDTO.cs
@@ -24,6 +24,7 @@
 
         public List<HerramientasParaOfertasDTO> herramienta { get; set; }
 
+
         public OfertasDTO()
         {
         }
