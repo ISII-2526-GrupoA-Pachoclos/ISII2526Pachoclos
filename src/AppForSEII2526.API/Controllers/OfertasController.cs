613bd569fe817f60471a7353de8dda5f2bc94dc6
ADRIÁN DANIEL MECINAS DUMITRU <AdrianDaniel.Mecinas@alu.uclm.es>
2025-10-21 16:33:54 +0200

    Realizado cambios en OfertasController y rellenadas las BBDD de Oferta y OfertaItem para realizar las pruebas

--- src/AppForSEII2526.API/Controllers/OfertasController.cs
+++ src/AppForSEII2526.API/Controllers/OfertasController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using System.Linq;
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -19,14 +20,14 @@ namespace AppForSEII2526.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet("GetDetalles_Oferta/{id}")]
+        [HttpGet]
         [Route("[action]")]
         [ProducesResponseType(typeof(IList<OfertaDetalleDTO>), (int)HttpStatusCode.OK)]
         public async Task<ActionResult> GetDetalles_Oferta(int id)
         {
             var ofertas = await _context.Oferta
                 .Where(o => o.Id == id)
-                .Include(o=> o.ofertaItems)
+                .Include(o => o.ofertaItems)
                     .ThenInclude(oi => oi.herramienta)
                 .Select(o => new OfertaDetalleDTO(
                     o.Id,
@@ -42,10 +43,20 @@ namespace AppForSEII2526.API.Controllers
                         oi.herramienta.fabricante.nombre,
                         oi.herramienta.precio,
                         oi.precioFinal
+                        //oi.herramienta.precio * (100f - oi.porcentaje) / 100f
                     )).ToList()
                 ))
                 .FirstOrDefaultAsync();
 
+            /*
+            if (ofertas == null)
+                return NotFound();
+
+            // Calculamos el total sumando los precioOferta ya calculados en la proyección.
+            var precioTotal = ofertas.HerramientasAOfertar?.Sum(i => i.precioOferta) ?? 0f;
+
+            return Ok(new { detalle = ofertas, precioTotal });
+            */
             return Ok(ofertas);
         }
     }
