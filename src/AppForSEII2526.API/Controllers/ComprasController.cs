c345e15a62d177f0f5f2e2338b1f465bb4b33a29
jesussotos <jesus.sotos@alu.uclm.es>
2025-10-22 12:25:38 +0200

    Método getDetalles creado (#130)

---
+++ src/AppForSEII2526.API/Controllers/ComprasController.cs
@@ -0,0 +1,58 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HerramientasController> _logger;
+
+        public ComprasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(CompraDetalleDTO), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetDetalles_Compra(int id)
+        {
+
+            var compra = await _context.Compra
+                .Where(c => c.Id == id)
+                .Include(c => c.ApplicationUser)
+                .Include(c => c.CompraItems)
+                    .ThenInclude(c => c.herramienta)
+                .Select(c => new CompraDetalleDTO(
+                    c.Id,
+                    c.direccionEnvio,
+                    //c.fechaCompra,
+                    c.precioTotal,
+                    c.CompraItems.Select(ci => new CompraItemDTO(
+                        ci.herramientaid,
+                        ci.cantidad,
+                        ci.herramienta.nombre,
+                        ci.descripcion,
+                        ci.herramienta.precio
+                    )).ToList()
+
+                 ))
+                .FirstOrDefaultAsync();
+            if (compra == null)
+            {
+                _logger.LogWarning("No se ha encontrado la compra con Id {Id}", id);
+                return NotFound();
+            }
+            return Ok(compra);
+
+
+
+
+        }
+    }
+}
--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -12,8 +12,11 @@
         [Required, StringLength(100, ErrorMessage = "No puede tener mas de 100 caracteres.", MinimumLength = 1)]
         public string direccionEnvio { get; set; }
 
+        [Required, DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime fechaCompra { get; set; }
 
-       
+
 
 
         [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
