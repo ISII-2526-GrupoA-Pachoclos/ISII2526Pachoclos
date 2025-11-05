78fe355d06b757dc518fc0baac1664840f8b797f
jesussotos <Jesus.Sotos@alu.uclm.es>
2025-11-05 12:40:24 +0100

    Atributos añadidos

--- src/AppForSEII2526.API/DTOs/CrearCompraDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearCompraDTO.cs
@@ -9,9 +9,17 @@ namespace AppForSEII2526.API.DTOs
 
         public string Apellido { get; set; }
 
+        [Required, StringLength(100, ErrorMessage = "El correo electrónico no puede tener más de 100 caracteres.")]
+        public string? correoElectronico { get; set; }
+
+        [StringLength(50, ErrorMessage = "No puede tener mas de 50 caracteres.", MinimumLength = 1)]
+        [RegularExpression(@"^\+?\d{10,15}$", ErrorMessage = "El número de teléfono no es válido. Ejemplo válido: 9876543210")]
+        public string? numTelefono { get; set; }
+
         public string direccionEnvio { get; set; }
 
         public formaPago metodoPago { get; set; }
+
         public IList<CompraItemDTO> HerramientasCompradas { get; set; }
     }
 }
