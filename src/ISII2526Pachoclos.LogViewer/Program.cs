40edc15e534e3505bfed504a00084e405716b717
MARCOS VILLALBA LÓPEZ <Marcos.Villalba@alu.uclm.es>
2025-11-13 13:05:43 +0100

    añadidos utilidades

--- src/ISII2526Pachoclos.LogViewer/Program.cs
+++ src/ISII2526Pachoclos.LogViewer/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Configuración
-        const string hostName = "localhost";
+        const string hostName = "172.20.10.3";
         const int port = 5672;
         const string userName = "guest";
         const string password = "guest";
