0d18fb82ac13f34adbc37dce61374a606495a057
ADRIÁN DANIEL MECINAS DUMITRU <AdrianDaniel.Mecinas@alu.uclm.es>
2025-12-01 10:57:41 +0100

    Iniciado seminario selenium. ultima diapositiva realizada (12)

---
+++ test/AppForSEII2526.UIT/CU-CrearOfertas/CUCrearOfertas_UIT.cs
@@ -0,0 +1,29 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_CrearOfertas
+{
+    public class CUCrearOfertas_UIT :IDisposable
+    {
+        IWebDriver _driver;
+        string _URI;
+        private readonly ITestOutputHelper _output;
+
+        public CUCrearOfertas_UIT(ITestOutputHelper output)
+        {
+            UC_UIT.SetUp_Chrome4UIT(out _driver, out _URI);
+            this._output = output;
+        }
+
+        void IDisposable.Dispose()
+        {
+            _driver.Close();
+            _driver.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
