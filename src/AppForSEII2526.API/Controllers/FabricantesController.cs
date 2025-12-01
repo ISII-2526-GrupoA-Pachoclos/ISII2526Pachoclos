5e253a614d40cd92f235de3f3089352e3aa56233
ADRIAN DANIEL MECINAS DUMITRU <53708631+AdrianDanielMecinasDumitru@users.noreply.github.com>
2025-12-01 17:26:27 +0100

    222 crear fabricantescontroller para la UI del select del cdu3 crear ofertas (#223)
    * Creado FabricantesController y modificado swagger1.json al actualizar el Servicio Conectado
    
    * Añadido en el selectHerramientasForOfertas.razor que solo usuarios autorizados puedan usar la app
    
    * Usado fabricantes como drop menú en la UI del select de ofertas

---
+++ src/AppForSEII2526.API/Controllers/FabricantesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        public FabricantesController(ApplicationDbContext context, ILogger<FabricantesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        //GET: /api/Fabricantes
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetFabricantes(string? fabricanteNombre)
+        {
+            IList<string> fabricantes = await _context.fabricante
+                .Where(f => (fabricanteNombre == null || f.nombre.Contains(fabricanteNombre)))
+                .OrderBy(f => f.nombre)
+                .Select(f => f.nombre)
+                .ToListAsync();
+
+            return Ok(fabricantes);
+        }
+    }
+}
