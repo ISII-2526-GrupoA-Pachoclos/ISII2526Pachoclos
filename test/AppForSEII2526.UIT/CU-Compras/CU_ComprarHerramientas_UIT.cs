25a029d9f14d5b87f93b92949f1f55c1a2751e55
jesussotos <Jesus.Sotos@alu.uclm.es>
2025-12-05 08:46:51 +0100

    Clases del CU implementadas

--- test/AppForSEII2526.UIT/CU-Compras/CU_ComprarHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/CU-Compras/CU_ComprarHerramientas_UIT.cs
@@ -7,28 +7,15 @@ using System.Threading.Tasks;
 
 namespace AppForSEII2526.UIT.CU_Compras
 {
-    internal class CU_ComprarHerramientas_UIT : IDisposable
+    internal class CU_ComprarHerramientas_UIT : UC_UIT
     {
-        public CU_ComprarHerramientas_UIT(ITestOutputHelper output)
+        public CU_ComprarHerramientas_UIT(ITestOutputHelper output):base(output)
         {
-            //it is needed to run the browser and
-            //know the URI of your app
-            //UC_UIT.SetUp_UIT(out _driver, out _URI);
-
-            //it is initialized using the logger provided by xUnit
-            this._output = output;
+            
+            
         }
-        IWebDriver _driver;
-        string _URI;
-        private readonly ITestOutputHelper _output;
+        
 
-        void IDisposable.Dispose()
-        {
-            //To close and release all the resources allocated by the web driver
-            _driver.Close();
-            _driver.Dispose();
-            GC.SuppressFinalize(this);
-
-        }
+        
     }
 }
---
+++ test/AppForSEII2526.UIT/CU-Compras/SelectHerramientasForCompraPO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Compras
+{
+    internal class SelectHerramientasForCompraPO: PageObject
+    {
+        public SelectHerramientasForCompraPO( IWebDriver driver, ITestOutputHelper output): base(driver, output) 
+        { 
+        
+        
+        }
+
+    }
+}
