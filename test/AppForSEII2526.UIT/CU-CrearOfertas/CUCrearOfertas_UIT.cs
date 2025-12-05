e8c5be3b1aa63e90b3ccd2599bc4899c6e00005b
ADRIÁN DANIEL MECINAS DUMITRU <AdrianDaniel.Mecinas@alu.uclm.es>
2025-12-05 09:48:23 +0100

    Iniciado pruebas uit del select

--- test/AppForSEII2526.UIT/CU-CrearOfertas/CUCrearOfertas_UIT.cs
+++ test/AppForSEII2526.UIT/CU-CrearOfertas/CUCrearOfertas_UIT.cs
@@ -7,23 +7,41 @@ using System.Threading.Tasks;
 
 namespace AppForSEII2526.UIT.CU_CrearOfertas
 {
-    public class CUCrearOfertas_UIT :IDisposable
+    public class CUCrearOfertas_UIT : UC_UIT
     {
-        IWebDriver _driver;
-        string _URI;
-        private readonly ITestOutputHelper _output;
+        private SelectHerramientasParaOfertasPO selectHerramientasParaOfertasPO;
+        private const string herramientaPrecio = "4";
+        private const string herramientaFabricante = "Ana";
 
-        public CUCrearOfertas_UIT(ITestOutputHelper output)
+        public CUCrearOfertas_UIT(ITestOutputHelper output) : base(output)
         {
-            UC_UIT.SetUp_Chrome4UIT(out _driver, out _URI);
-            this._output = output;
+            selectHerramientasParaOfertasPO = new SelectHerramientasParaOfertasPO(_driver, _output);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
-        void IDisposable.Dispose()
+        private void InitialStepsForOfertarHerramientas()
         {
-            _driver.Close();
-            _driver.Dispose();
-            GC.SuppressFinalize(this);
+            
+            // Esperar a que el menú de navegación se cargue
+            selectHerramientasParaOfertasPO.WaitForBeingVisible(By.Id("CreateOfertas"));
+
+            // Hacer clic en el menú
+            _driver.FindElement(By.Id("CreateOfertas")).Click();
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_AF1_UC2_4_5_6_filtering()
+        {
+            //Arrange
+            InitialStepsForOfertarHerramientas();
+            var expectedHerramientas = new List<string[]> { new string[] { herramientaPrecio, herramientaFabricante }, };
+
+            //Act
+            selectHerramientasParaOfertasPO.BuscarHerramientas("2", "");
+
+            //Assert
+            Assert.True(selectHerramientasParaOfertasPO.CheckListOfHerramientas(expectedHerramientas));
         }
     }
 }
---
+++ test/AppForSEII2526.UIT/CU-CrearOfertas/SelectHerramientasParaOfertasPO.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium.DevTools.V137.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_CrearOfertas
+{
+    public class SelectHerramientasParaOfertasPO : PageObject
+    {
+        private By inputPrecio = By.Id("inputTitle");
+        private By inputFabricante = By.Id("fabricanteSelect");
+        private By buttonBuscarHerramientas = By.Id("searchHerramientas");
+        private By tableOfHerramientasBy = By.Id("TableOfHerramientas");
+
+        public SelectHerramientasParaOfertasPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+            
+        }
+
+        public void BuscarHerramientas(string precio, string fabricante)
+        {
+            WaitForBeingClickable(inputPrecio);
+            _driver.FindElement(inputPrecio).SendKeys(precio);
+
+            if (fabricante == "") fabricante = "All";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(inputFabricante));
+            selectElement.SelectByText(fabricante);
+
+            _driver.FindElement(buttonBuscarHerramientas).Click();
+        }
+
+        public bool CheckListOfHerramientas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, tableOfHerramientasBy);
+        }
+    }
+}
