7edc5dfed338cbbdf6297e2a155268d50014170c
jesussotos <Jesus.Sotos@alu.uclm.es>
2025-12-05 09:38:00 +0100

    Pruebas parcialmente Implementadas

--- test/AppForSEII2526.UIT/CU-Compras/CU_ComprarHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/CU-Compras/CU_ComprarHerramientas_UIT.cs
@@ -7,15 +7,48 @@ using System.Threading.Tasks;
 
 namespace AppForSEII2526.UIT.CU_Compras
 {
-    internal class CU_ComprarHerramientas_UIT : UC_UIT
+    public class CU_ComprarHerramientas_UIT : UC_UIT
     {
+
+        private SelectHerramientasForCompraPO selectHerramientasForCompraPO;
+
+        private const string NombreHerramienta = "Martillo";
+        private const string FabricanteHerramienta = "Jose";
+        private const string MaterialHerramienta = "Madera";
+        private const int PrecioHerramienta = 6;
+
         public CU_ComprarHerramientas_UIT(ITestOutputHelper output):base(output)
         {
-            
-            
-        }
-        
+            selectHerramientasForCompraPO = new SelectHerramientasForCompraPO(_driver, _output);
 
-        
+
+
+        }
+
+        private void InitialStepsForCompra() {
+
+            selectHerramientasForCompraPO.WaitForBeingVisible(By.Id("CrearCompra"));
+
+            _driver.FindElement(By.Id("CrearCompra")).Click();
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CU1_4_5_FA1_filtrarbyMaterial() { 
+
+            InitialStepsForCompra();
+            var expectedHerramientas = new List<string[]>
+            {
+                new string[] { NombreHerramienta, FabricanteHerramienta, MaterialHerramienta, PrecioHerramienta.ToString() }
+            };
+
+            selectHerramientasForCompraPO.BuscarHerramientas("Made", 0);
+
+            Assert.True(selectHerramientasForCompraPO.CheckListaHerramientas(expectedHerramientas));
+        }
+
+
+
+
     }
 }
--- test/AppForSEII2526.UIT/CU-Compras/SelectHerramientasForCompraPO.cs
+++ test/AppForSEII2526.UIT/CU-Compras/SelectHerramientasForCompraPO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium.DevTools.V137.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,32 @@ namespace AppForSEII2526.UIT.CU_Compras
     {
         public SelectHerramientasForCompraPO( IWebDriver driver, ITestOutputHelper output): base(driver, output) 
         { 
-        
-        
+            
+
+
+
         }
+        By filtroPrecio = By.Id("filtroPrecio");
+        By filtroMaterial = By.Id("filtroMaterial");
+        By botonBuscar = By.Id("buscarHerramientas");
+        By tablaHerramientasBy = By.Id("Tabla de Herramientas");
+
+        public void BuscarHerramientas(string material, int percio) {
+            WaitForBeingClickable(filtroMaterial);
+
+            _driver.FindElement(filtroMaterial).SendKeys(material);
+            _driver.FindElement(filtroPrecio).SendKeys(percio.ToString());
+            _driver.FindElement(botonBuscar).Click();
+
+
+        }
+
+        public bool CheckListaHerramientas(List<string[]> expectedHerramientas) { 
+
+            return CheckBodyTable(expectedHerramientas, tablaHerramientasBy);
+        }
+
+        
 
     }
 }
