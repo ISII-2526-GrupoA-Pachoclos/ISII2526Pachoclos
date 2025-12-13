45c19ebf40265871d751ab75c1e33bf856b90910
ADRIAN DANIEL MECINAS DUMITRU <53708631+AdrianDanielMecinasDumitru@users.noreply.github.com>
2025-12-13 18:16:24 +0100

    224 pruebas funcionales con selenium de la interfaces del CU3 Crear Ofertas (#253)
    * Iniciado seminario selenium. ultima diapositiva realizada (12)
    
    * Iniciado pruebas uit del select
    
    * Creadas pruebas select theory para filtrar por precio y/o fabricante
    
    * Realizadas pruebas de la interfaz del select de Crear Ofertas
    
    * CREADAS LAS PRUEBAS DE LA INTERFAZ DEL SELECT DEL CU3 CREAR OFERTAS
    
    PD: No se hasta cuanto va a ir el sleep
    
    * Añadido scroll hacia abajo al añadir la herramienta para que se vea las herramientas añadidas al carrito y se puedan eliminar
    
    * Cambios estéticos en la separación de las pruebas del select
    
    * Iniciado creación de pruebas del post
    
    * Creadas las pruebas de las interfaces del select, detail y post del CU3 Crear ofertas
    
    * Subido documento Casos de Prueba del CU3 Crear Ofertas

---
+++ test/AppForSEII2526.UIT/CU-CrearOfertas/DetalleOfertaPO.cs
@@ -0,0 +1,40 @@
+﻿using AppForSEII2526.UIT.Shared;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+
+namespace AppForSEII2526.UIT.CU_CrearOfertas
+{
+    public class DetalleOfertaPO : PageObject
+    {
+        private By botonPrecioTotal = By.Id("PrecioTotal");
+
+        public DetalleOfertaPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+
+        }
+
+        public bool CheckOfertaDetail(DateTime fechaInicio, DateTime fechaFin, DateTime fechaOferta, string metodoPago, string dirigidaA, int ofertaItems, float precioTotal)
+        {
+            WaitForBeingVisible(botonPrecioTotal);
+            bool result = true;
+
+            result = result && _driver.FindElement(By.Id("FechaInicio")).Text.Contains(fechaInicio.ToString("dd/MM/yyyy"));
+            result = result && _driver.FindElement(By.Id("FechaFin")).Text.Contains(fechaFin.ToString("dd/MM/yyyy"));
+            result = result && _driver.FindElement(By.Id("FechaOferta")).Text.Contains(fechaOferta.ToString("dd/MM/yyyy"));
+            result = result && _driver.FindElement(By.Id("MetodoPago")).Text.Contains(metodoPago);
+            result = result && _driver.FindElement(By.Id("TiposDirigidaOferta")).Text.Contains(dirigidaA);
+            result = result && _driver.FindElement(By.Id("OfertaItems")).Text.Contains(ofertaItems.ToString());
+            result = result && _driver.FindElement(botonPrecioTotal).Text.Contains(precioTotal.ToString("F2"));
+
+            return result;
+        }
+
+        public bool CheckListOfHerramientasOfertadas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, By.Id("DetalleOferta"));
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/CU-CrearOfertas/SelectHerramientasParaOfertasPO.cs
@@ -0,0 +1,105 @@
+﻿using AppForSEII2526.UIT.Shared;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Linq;
+using Xunit.Abstractions;
+
+namespace AppForSEII2526.UIT.CU_CrearOfertas
+{
+    public class SelectHerramientasParaOfertasPO : PageObject
+    {
+        private By inputPrecio = By.Id("inputTitle");
+        private By inputFabricante = By.Id("fabricanteSelected");
+        private By buttonBuscarHerramientas = By.Id("searchHerramientas");
+        private By tableOfHerramientasBy = By.Id("TableOfHerramientas");
+        private By botonOfertar = By.Id("ofertarHerramientaButton");
+
+        public SelectHerramientasParaOfertasPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+
+        }
+
+        public void BuscarHerramientas(string precio, string fabricante)
+        {
+            WaitForBeingClickable(inputPrecio);
+            _driver.FindElement(inputPrecio).SendKeys(precio);
+
+            if (fabricante == "") fabricante = "All";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(inputFabricante));
+            selectElement.SelectByText(fabricante);
+
+            _driver.FindElement(buttonBuscarHerramientas).Click();
+        }
+
+        public bool CheckListOfHerramientas(List<string[]> expectedHerramientas)
+        {
+
+            return CheckBodyTable(expectedHerramientas, tableOfHerramientasBy);
+        }
+
+        public void AddHerramientaToCarrito(string nombreHerramienta)
+        {
+            By addButton = By.Id("herramientaToOfertar_" + nombreHerramienta);
+            WaitForBeingClickable(addButton);
+            _driver.FindElement(addButton).Click();
+
+            // Hacer scroll hacia el botón de ofertar usando Actions
+            WaitForBeingVisible(botonOfertar);
+            IWebElement botonOfertarElement = _driver.FindElement(botonOfertar);
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(botonOfertarElement).Perform();
+        }
+
+        public void RemoveHerramientaFromCarrito(string nombreHerramienta)
+        {
+            By removeButton = By.Id("removeHerramienta_" + nombreHerramienta);
+            WaitForBeingClickable(removeButton);
+            
+            // Hacer scroll hacia el botón de eliminar primero
+            IWebElement removeButtonElement = _driver.FindElement(removeButton);
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(removeButtonElement).Perform();
+            Thread.Sleep(300); // Pequeña pausa para que termine el scroll
+            
+            try
+            {
+                // Intentar click normal
+                removeButtonElement.Click();
+            }
+            catch (ElementClickInterceptedException)
+            {
+                // Si falla, usar JavaScript para hacer click
+                IJavaScriptExecutor js = (IJavaScriptExecutor)_driver;
+                js.ExecuteScript("arguments[0].click();", removeButtonElement);
+            }
+
+            // Hacer scroll hacia el botón de ofertar después de eliminar
+            WaitForBeingVisible(botonOfertar);
+            IWebElement botonOfertarElement = _driver.FindElement(botonOfertar);
+            actions.MoveToElement(botonOfertarElement).Perform();
+        }
+
+        public bool OfertarHerramientasNotAvailable()
+        {
+            // The button is not Displayed == hidden
+            try
+            {
+                return _driver.FindElement(botonOfertar).Displayed == false;
+            }
+            catch (Exception)
+            {
+                return true; // Si no existe, está oculto
+            }
+        }
+
+        public void ClickOfertarHerramientas()
+        {
+            WaitForBeingClickable(botonOfertar);
+            _driver.FindElement(botonOfertar).Click();
+        }
+    }
+}
